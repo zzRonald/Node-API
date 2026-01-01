65b8fd1346f7aee8f04ea85258a9510c31a12ad6
zzRonald <dev.ronaldd@gmail.com>
2026-01-01 01:13:36 -0300

    Atualiza rota principal e request.http

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -1,18 +1,27 @@
 import { Router } from "express" 
+
+
+const users = []
  export const mainroute = Router () 
 
- mainroute.get("/test", (request, response)=> {
-    return response.status(200).send({ message: "OK"})
-})
-
-mainroute.get("/", (request, response)=> {
-    return response.status(200).send({ message: "SEJA BEM VINDO!!"})
-    
-})
-
-mainroute.post("/Login", (request, response)=> {
+mainroute.post("/sign-up", (request, response)=> {
     const { email, password } = request.body
 
-    return response.status(200).send({ body: { email, password }})
+    if(!email) {
+        return response.status(400).send({ message: "Email é obrigatório" })
+    }
+    if(!password) {
+        return response.status(400).send({ message: "Senha é obrigatória" })
+    }
+const usersExists = users.find(item => item.email == email)
+    if(usersExists) {
+        return response.status(409).send({ message: "Usuarios já cadastrado" })
+    }   
+
+
+users.push({ email, password })
+
+
+    return response.status(201).send({ users })
     
 }) 
